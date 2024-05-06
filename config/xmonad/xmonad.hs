3276fa45e139a660238bfd05e8a8e66d2193c516
Minerte <169073521+Minerte@users.noreply.github.com>
2024-05-06 18:48:13 +0200

    Update xmonad.hs

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -246,7 +246,7 @@ myLogHook = return ()
 myStartupHook = do
 	spawnOnce "picom --experimental-backend"
 	spawnOnce "polybar middle"
-	spawnOnce "feh --bg-fill /home/$(user)/Downloads/wallie.png"
+	spawnOnce "feh --bg-fill /path/to/image"
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
 
